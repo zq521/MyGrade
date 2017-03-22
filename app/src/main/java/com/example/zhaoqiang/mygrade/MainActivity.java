6b4318a9897e78539709e131263da2430a9269cd
zhaoqiang <727319870@qq.com>
2017-03-22 16:48:46 +0800

    Initial commit

--- app/src/main/java/com/example/zhaoqiang/mygrade/MainActivity.java
+++ app/src/main/java/com/example/zhaoqiang/mygrade/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.zhaoqiang.mygrade;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.hyphenate.EMCallBack;
+import com.hyphenate.chat.EMClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        EMClient.getInstance().login("xhz","727319870",new EMCallBack() {//回调
+            @Override
+            public void onSuccess() {
+                EMClient.getInstance().groupManager().loadAllGroups();
+                EMClient.getInstance().chatManager().loadAllConversations();
+                Log.d("main", "登录聊天服务器成功！");
+            }
+
+            @Override
+            public void onProgress(int progress, String status) {
+
+            }
+
+            @Override
+            public void onError(int code, String message) {
+                Log.d("main", "登录聊天服务器失败！");
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/example/zhaoqiang/mygrade/MyApplication.java
@@ -0,0 +1,29 @@
+package com.example.zhaoqiang.mygrade;
+
+import android.app.Application;
+
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMOptions;
+
+/**
+ * Created by 轩韩子 on 2017/3/22.
+ * at 16:06
+ */
+
+public class MyApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        EMOptions options = new EMOptions();
+// 默认添加好友时，是不需要验证的，改成需要验证
+        options.setAcceptInvitationAlways(false);
+
+//初始化
+        EMClient.getInstance().init(this, options);
+//在做打包混淆时，关闭debug模式，避免消耗不必要的资源
+        EMClient.getInstance().setDebugMode(true);
+
+
+    }
+}
