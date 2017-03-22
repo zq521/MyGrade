2ae8274f192e369664fe99883963bf1e7571a6ac
zhaoqiang <727319870@qq.com>
2017-03-22 16:01:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zhaoqiang/mygrade/act/Baseactivity.java
@@ -0,0 +1,17 @@
+package com.example.zhaoqiang.mygrade.act;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 轩韩子 on 2017/3/22.
+ * at 14:04
+ */
+
+public class Baseactivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
